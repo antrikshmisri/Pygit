0ce0217e576bbc612179c3d0d02f48f3229fcccf
antrikshmisri <antrikshmisri@gmail.com>
2021-05-31 00:12:54 +0530

    fix: fixed classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 classifiers = [
-  'Development Status :: 6 - Production/Stable',
+  'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'License :: OSI Approved :: MIT License',
